b88462d6ef67cbff1a9d04112d953cc5556dfc3d
Mohammad Wasi <reallywasi@gmail.com>
2023-11-14 18:36:25 +0530

    Update Header.jsx

--- Router/Header.jsx
+++ Router/Header.jsx
@@ -1,1 +1,26 @@
 
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+import './Header.css';
+const Header = () => {
+  return (
+    <div>
+       <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+    </div>
+  )
+}
+
+export default Header
