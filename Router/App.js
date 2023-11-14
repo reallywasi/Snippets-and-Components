fef50410dd1b964a9b7eae52852966969dc79ead
Mohammad Wasi <reallywasi@gmail.com>
2023-11-14 18:37:09 +0530

    Update App.js

--- Router/App.js
+++ Router/App.js
@@ -1,1 +1,30 @@
 
+import React from 'react'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+
+import Header from './components/Header'
+import Contact from './components/Contact'
+import Home from './components/Home'
+import About from './components/About'
+const App = () => {
+  return (
+    <div>
+    
+     <Router>
+     <div>
+     <Header/>
+
+     </div>
+
+     <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+     </Router>
+    </div>
+  )
+}
+
+export default App
+
