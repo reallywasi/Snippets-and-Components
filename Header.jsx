62a17c225056081a1d38677feb3ab3f4108d1cb4
Mohammad Wasi <reallywasi@gmail.com>
2023-11-18 22:52:18 +0530

    Create Header.jsx

---
+++ Header.jsx
@@ -0,0 +1,35 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import './BasicExample.css'; // Correct way to import the CSS file
+function BasicExample() {
+  return (
+    <Navbar expand="lg"  className='navstyle' style={{color:'white'}}>
+      <Container>
+        <Navbar.Brand href="#home">Wings Consultants</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className='hell'>Home</Nav.Link>
+            <Nav.Link href="#link">About us</Nav.Link>
+            <Nav.Link href="#link">Recruit</Nav.Link>
+            <NavDropdown title="Jobs" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
