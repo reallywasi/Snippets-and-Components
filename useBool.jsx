2135e92bb28af19b33f9eefc9d0b3952e4fabb6a
Mohammad Wasi <reallywasi@gmail.com>
2023-12-01 20:17:53 +0530

    Update useBool.jsx

--- useBool.jsx
+++ useBool.jsx
@@ -1,4 +1,4 @@
-Example 1 .
+//Example 1 .
   
 
 function Example() {
@@ -8,5 +8,22 @@ function Example() {
     <div onMouseEnter={setFlag.on} onMouseLeave={setFlag.off}>
       {flag ? 'The flag is ON!' : 'Hover me to turn ON'}
     </div>
+  )
+}
+
+
+
+
+//Example 2.
+function Example() {
+  const [flag, setFlag] = useBoolean()
+
+  return (
+    <>
+      <p>Boolean state: {flag.toString()}</p>
+      <button onClick={setFlag.toggle}>
+        Click me to toggle the boolean value
+      </button>
+    </>
   )
 }
