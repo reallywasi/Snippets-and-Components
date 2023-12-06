f3631d15982454950b0df6fdba95892508a41e0f
Mohammad Wasi <reallywasi@gmail.com>
2023-12-06 11:29:36 +0530

    Update Expresshtmlsend.js

--- Expresshtmlsend.js
+++ Expresshtmlsend.js
@@ -1,1 +1,14 @@
 
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
